027fa5ab5bbc11785d6280b1b3f42b03fefbbca8
mcsbogo <mcsbo.go@gmx.de>
2026-07-12 00:04:23 +0200

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import './auth.css';
 if ('serviceWorker' in navigator) navigator.serviceWorker.register('/sw.js');
 createRoot(document.getElementById('root')).render(<App />);
